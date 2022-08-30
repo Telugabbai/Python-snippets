bc3607cce3af09ebb41f8a94063feed034c3b00f
Telugabbai <mekalasreenivasarao8@gmail.com>
2022-08-30 21:29:46 +0530

    caesar

--- core/Cipher.py
+++ core/Cipher.py
@@ -8,25 +8,22 @@ def caesar():
     direction = input(" Enter 'encode' for encryption and 'decode' for decryption ")
     msg = input("Enter the message: ").lower()
     shift = int(input("Enter the shift: "))
-
     cipher_text = ""
+    shift_amount = 0
 
     if direction == 'encode':
-        for letter in msg:
-            msg_char = alphabet.index(letter)
-            msg_char += shift
-            new_char_pos = alphabet[msg_char]
-            cipher_text = cipher_text+new_char_pos
-        print(f"Your Encoded cipher text is : {cipher_text} ")
+        shift_amount += shift
     elif direction == 'decode':
-        for letter in msg:
-            msg_char = alphabet.index(letter)
-            msg_char -= shift
-            new_char_pos = alphabet[msg_char]
-            cipher_text = cipher_text+new_char_pos
-        print(f"Your Decoded cipher text is : {cipher_text} ")
+        shift_amount -= shift
     else:
         print(" Enter valid option")
+
+    for letter in msg:
+        msg_char = alphabet.index(letter)
+        msg_char += shift_amount
+        new_char_pos = alphabet[msg_char]
+        cipher_text = cipher_text+new_char_pos
+    print(f"Your cipher text is : {cipher_text} ")
 
 
 caesar()
