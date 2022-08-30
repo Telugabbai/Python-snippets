9e7e7523ae5cdd6880bc483e7108596d069dd257
Telugabbai <mekalasreenivasarao8@gmail.com>
2022-08-30 20:47:49 +0530

    caesar func

--- core/Cipher.py
+++ core/Cipher.py
@@ -1,23 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input(" Enter 'encode' for encryption and 'decode' for decryption ")
-msg = input("Enter the message: ").lower()
-shift = int(input("Enter the shift: "))
+def caesar():
 
-cipher_text = ""
-if direction == 'encode':
-    for letter in msg:
-        msg_char = alphabet.index(letter)
-        msg_char += shift
-        new_char_pos = alphabet[msg_char]
-        cipher_text = cipher_text+new_char_pos
-elif direction == 'decode':
-    for letter in msg:
-        msg_char = alphabet.index(letter)
-        msg_char -= shift
-        new_char_pos = alphabet[msg_char]
-        cipher_text = cipher_text+new_char_pos
-else:
-    print(" Enter valid option")
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-print(cipher_text)
+    direction = input(" Enter 'encode' for encryption and 'decode' for decryption ")
+    msg = input("Enter the message: ").lower()
+    shift = int(input("Enter the shift: "))
+
+    cipher_text = ""
+    if direction == 'encode':
+        for letter in msg:
+            msg_char = alphabet.index(letter)
+            msg_char += shift
+            new_char_pos = alphabet[msg_char]
+            cipher_text = cipher_text+new_char_pos
+        print(f"Your Encoded cipher text is : {cipher_text} ")
+    elif direction == 'decode':
+        for letter in msg:
+            msg_char = alphabet.index(letter)
+            msg_char -= shift
+            new_char_pos = alphabet[msg_char]
+            cipher_text = cipher_text+new_char_pos
+        print(f"Your Decoded cipher text is : {cipher_text} ")
+    else:
+        print(" Enter valid option")
