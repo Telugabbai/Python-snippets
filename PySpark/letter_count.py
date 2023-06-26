f86af60bfc5cd13d21b304037a12f46a02ebfc1a
admin <mekalasreenivasarao8@gmail.com>
2023-06-26 19:04:16 +0530

    letter count updated

--- PySpark/letter_count.py
+++ PySpark/letter_count.py
@@ -1,21 +1,28 @@
-import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count
+from pyspark.sql.functions import col, lower, regexp_replace
+from pyspark.sql.functions import split, explode, size, count
 
 # Create a SparkSession
-spark = SparkSession.builder.appName("Count Letters").getOrCreate()
+spark = SparkSession.builder.appName("LetterCount").getOrCreate()
 
-# Create a DataFrame from the phrase
-df = spark.createDataFrame(["it's not who i am underneath but what i do that defines me"], "string").toDF("phrase")
+# Enable case sensitivity
+spark.conf.set("spark.sql.caseSensitive", "true")
 
-# Count the number of each letter in the phrase
-letter_counts = df.select(col("phrase").split("").alias("letter")).groupBy("letter").count()
+# Create a DataFrame with the input text
+input_text = "its not who i am underneath but what i do that defines me"
+df = spark.createDataFrame([(input_text,)], ['text'])
 
+# Remove non-alphabetic characters and convert to lowercase
+clean_text = df.withColumn("clean_text", lower(regexp_replace(col("text"), "[^a-z]", "")))
 
-# Count the no.of words
-word_counts = df.select(col("phrase").split(" ").alias("Word")).groupBy("Word").count()
+# Split the clean text into individual letters
+letter_df = clean_text.withColumn("letters", split(col("clean_text"), ""))
 
-# Print the letter counts
+# Explode the letters array into separate rows
+exploded_df = letter_df.select(explode(col("letters")).alias("letter"))
+
+# Count the occurrences of each letter
+letter_counts = exploded_df.groupBy("letter").agg(count("*").alias("count")).orderBy("letter")
+
+# Show the results
 letter_counts.show()
-
-word_counts.show()
